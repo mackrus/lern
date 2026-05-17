dbb2b89c7286ad68fef8376ed64d2b30d13b3c40
mackrus <mackrus@users.noreply.github.com>
2026-05-17 21:23:32 +0000

    deploy: 138c3f784047f4b31bd45f51112fdb132ff662f6

---
+++ pkg/lern.d.ts
@@ -0,0 +1,126 @@
+/* tslint:disable */
+/* eslint-disable */
+
+export function can_go_back(): boolean;
+
+export function get_alternative_html(index: number): string | undefined;
+
+export function get_alternatives_count(): number;
+
+export function get_current_question_explanation_html(): string | undefined;
+
+export function get_current_question_explanation_raw(): string | undefined;
+
+export function get_current_question_html(): string | undefined;
+
+export function get_current_question_id(): string | undefined;
+
+export function get_current_question_index(): number;
+
+export function get_current_question_label(): string | undefined;
+
+export function get_current_question_prerequisites_html(): string | undefined;
+
+export function get_current_question_raw(): string | undefined;
+
+export function get_current_question_references_json(): string | undefined;
+
+export function get_current_selection(): number | undefined;
+
+export function get_explanation_html_by_index(index: number): string | undefined;
+
+export function get_incorrect_indices(): Uint32Array;
+
+export function get_question_html_by_index(index: number): string | undefined;
+
+export function get_references_json_by_index(index: number): string | undefined;
+
+export function get_score(): number;
+
+export function get_selections_json(): string | undefined;
+
+export function get_topic_stats_json(): string | undefined;
+
+export function get_total_questions(): number;
+
+export function grade_quiz(): void;
+
+export function init_quiz(questions_json: string): void;
+
+export function is_finished(): boolean;
+
+export function is_graded(): boolean;
+
+export function next_question(): void;
+
+export function previous_question(): void;
+
+export function restore_quiz_state(current_index: number, selections_json: string, is_graded: boolean): void;
+
+export function select_answer(index: number): void;
+
+export function set_question_index(index: number): void;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+    readonly memory: WebAssembly.Memory;
+    readonly can_go_back: () => number;
+    readonly get_alternative_html: (a: number) => [number, number];
+    readonly get_alternatives_count: () => number;
+    readonly get_current_question_explanation_html: () => [number, number];
+    readonly get_current_question_explanation_raw: () => [number, number];
+    readonly get_current_question_html: () => [number, number];
+    readonly get_current_question_id: () => [number, number];
+    readonly get_current_question_label: () => [number, number];
+    readonly get_current_question_prerequisites_html: () => [number, number];
+    readonly get_current_question_raw: () => [number, number];
+    readonly get_current_question_references_json: () => [number, number];
+    readonly get_current_selection: () => number;
+    readonly get_explanation_html_by_index: (a: number) => [number, number];
+    readonly get_incorrect_indices: () => [number, number];
+    readonly get_question_html_by_index: (a: number) => [number, number];
+    readonly get_references_json_by_index: (a: number) => [number, number];
+    readonly get_score: () => number;
+    readonly get_selections_json: () => [number, number];
+    readonly get_topic_stats_json: () => [number, number];
+    readonly get_total_questions: () => number;
+    readonly init_quiz: (a: number, b: number) => [number, number];
+    readonly is_finished: () => number;
+    readonly is_graded: () => number;
+    readonly restore_quiz_state: (a: number, b: number, c: number, d: number) => [number, number];
+    readonly grade_quiz: () => void;
+    readonly get_current_question_index: () => number;
+    readonly previous_question: () => void;
+    readonly select_answer: (a: number) => void;
+    readonly next_question: () => void;
+    readonly set_question_index: (a: number) => void;
+    readonly __wbindgen_externrefs: WebAssembly.Table;
+    readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+    readonly __wbindgen_malloc: (a: number, b: number) => number;
+    readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+    readonly __externref_table_dealloc: (a: number) => void;
+    readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+
+/**
+ * Instantiates the given `module`, which can either be bytes or
+ * a precompiled `WebAssembly.Module`.
+ *
+ * @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+ *
+ * @returns {InitOutput}
+ */
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+ * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+ * for everything else, calls `WebAssembly.instantiate` directly.
+ *
+ * @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+ *
+ * @returns {Promise<InitOutput>}
+ */
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ pkg/lern.js
@@ -0,0 +1,527 @@
+/* @ts-self-types="./lern.d.ts" */
+
+/**
+ * @returns {boolean}
+ */
+export function can_go_back() {
+    const ret = wasm.can_go_back();
+    return ret !== 0;
+}
+
+/**
+ * @param {number} index
+ * @returns {string | undefined}
+ */
+export function get_alternative_html(index) {
+    const ret = wasm.get_alternative_html(index);
+    let v1;
+    if (ret[0] !== 0) {
+        v1 = getStringFromWasm0(ret[0], ret[1]).slice();
+        wasm.__wbindgen_free(ret[0], ret[1] * 1, 1);
+    }
+    return v1;
+}
+
+/**
+ * @returns {number}
+ */
+export function get_alternatives_count() {
+    const ret = wasm.get_alternatives_count();
+    return ret >>> 0;
+}
+
+/**
+ * @returns {string | undefined}
+ */
+export function get_current_question_explanation_html() {
+    const ret = wasm.get_current_question_explanation_html();
+    let v1;
+    if (ret[0] !== 0) {
+        v1 = getStringFromWasm0(ret[0], ret[1]).slice();
+        wasm.__wbindgen_free(ret[0], ret[1] * 1, 1);
+    }
+    return v1;
+}
+
+/**
+ * @returns {string | undefined}
+ */
+export function get_current_question_explanation_raw() {
+    const ret = wasm.get_current_question_explanation_raw();
+    let v1;
+    if (ret[0] !== 0) {
+        v1 = getStringFromWasm0(ret[0], ret[1]).slice();
+        wasm.__wbindgen_free(ret[0], ret[1] * 1, 1);
+    }
+    return v1;
+}
+
+/**
+ * @returns {string | undefined}
+ */
+export function get_current_question_html() {
+    const ret = wasm.get_current_question_html();
+    let v1;
+    if (ret[0] !== 0) {
+        v1 = getStringFromWasm0(ret[0], ret[1]).slice();
+        wasm.__wbindgen_free(ret[0], ret[1] * 1, 1);
+    }
+    return v1;
+}
+
+/**
+ * @returns {string | undefined}
+ */
+export function get_current_question_id() {
+    const ret = wasm.get_current_question_id();
+    let v1;
+    if (ret[0] !== 0) {
+        v1 = getStringFromWasm0(ret[0], ret[1]).slice();
+        wasm.__wbindgen_free(ret[0], ret[1] * 1, 1);
+    }
+    return v1;
+}
+
+/**
+ * @returns {number}
+ */
+export function get_current_question_index() {
+    const ret = wasm.get_current_question_index();
+    return ret >>> 0;
+}
+
+/**
+ * @returns {string | undefined}
+ */
+export function get_current_question_label() {
+    const ret = wasm.get_current_question_label();
+    let v1;
+    if (ret[0] !== 0) {
+        v1 = getStringFromWasm0(ret[0], ret[1]).slice();
+        wasm.__wbindgen_free(ret[0], ret[1] * 1, 1);
+    }
+    return v1;
+}
+
+/**
+ * @returns {string | undefined}
+ */
+export function get_current_question_prerequisites_html() {
+    const ret = wasm.get_current_question_prerequisites_html();
+    let v1;
+    if (ret[0] !== 0) {
+        v1 = getStringFromWasm0(ret[0], ret[1]).slice();
+        wasm.__wbindgen_free(ret[0], ret[1] * 1, 1);
+    }
+    return v1;
+}
+
+/**
+ * @returns {string | undefined}
+ */
+export function get_current_question_raw() {
+    const ret = wasm.get_current_question_raw();
+    let v1;
+    if (ret[0] !== 0) {
+        v1 = getStringFromWasm0(ret[0], ret[1]).slice();
+        wasm.__wbindgen_free(ret[0], ret[1] * 1, 1);
+    }
+    return v1;
+}
+
+/**
+ * @returns {string | undefined}
+ */
+export function get_current_question_references_json() {
+    const ret = wasm.get_current_question_references_json();
+    let v1;
+    if (ret[0] !== 0) {
+        v1 = getStringFromWasm0(ret[0], ret[1]).slice();
+        wasm.__wbindgen_free(ret[0], ret[1] * 1, 1);
+    }
+    return v1;
+}
+
+/**
+ * @returns {number | undefined}
+ */
+export function get_current_selection() {
+    const ret = wasm.get_current_selection();
+    return ret === Number.MAX_SAFE_INTEGER ? undefined : ret;
+}
+
+/**
+ * @param {number} index
+ * @returns {string | undefined}
+ */
+export function get_explanation_html_by_index(index) {
+    const ret = wasm.get_explanation_html_by_index(index);
+    let v1;
+    if (ret[0] !== 0) {
+        v1 = getStringFromWasm0(ret[0], ret[1]).slice();
+        wasm.__wbindgen_free(ret[0], ret[1] * 1, 1);
+    }
+    return v1;
+}
+
+/**
+ * @returns {Uint32Array}
+ */
+export function get_incorrect_indices() {
+    const ret = wasm.get_incorrect_indices();
+    var v1 = getArrayU32FromWasm0(ret[0], ret[1]).slice();
+    wasm.__wbindgen_free(ret[0], ret[1] * 4, 4);
+    return v1;
+}
+
+/**
+ * @param {number} index
+ * @returns {string | undefined}
+ */
+export function get_question_html_by_index(index) {
+    const ret = wasm.get_question_html_by_index(index);
+    let v1;
+    if (ret[0] !== 0) {
+        v1 = getStringFromWasm0(ret[0], ret[1]).slice();
+        wasm.__wbindgen_free(ret[0], ret[1] * 1, 1);
+    }
+    return v1;
+}
+
+/**
+ * @param {number} index
+ * @returns {string | undefined}
+ */
+export function get_references_json_by_index(index) {
+    const ret = wasm.get_references_json_by_index(index);
+    let v1;
+    if (ret[0] !== 0) {
+        v1 = getStringFromWasm0(ret[0], ret[1]).slice();
+        wasm.__wbindgen_free(ret[0], ret[1] * 1, 1);
+    }
+    return v1;
+}
+
+/**
+ * @returns {number}
+ */
+export function get_score() {
+    const ret = wasm.get_score();
+    return ret >>> 0;
+}
+
+/**
+ * @returns {string | undefined}
+ */
+export function get_selections_json() {
+    const ret = wasm.get_selections_json();
+    let v1;
+    if (ret[0] !== 0) {
+        v1 = getStringFromWasm0(ret[0], ret[1]).slice();
+        wasm.__wbindgen_free(ret[0], ret[1] * 1, 1);
+    }
+    return v1;
+}
+
+/**
+ * @returns {string | undefined}
+ */
+export function get_topic_stats_json() {
+    const ret = wasm.get_topic_stats_json();
+    let v1;
+    if (ret[0] !== 0) {
+        v1 = getStringFromWasm0(ret[0], ret[1]).slice();
+        wasm.__wbindgen_free(ret[0], ret[1] * 1, 1);
+    }
+    return v1;
+}
+
+/**
+ * @returns {number}
+ */
+export function get_total_questions() {
+    const ret = wasm.get_total_questions();
+    return ret >>> 0;
+}
+
+export function grade_quiz() {
+    wasm.grade_quiz();
+}
+
+/**
+ * @param {string} questions_json
+ */
+export function init_quiz(questions_json) {
+    const ptr0 = passStringToWasm0(questions_json, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
+    const len0 = WASM_VECTOR_LEN;
+    const ret = wasm.init_quiz(ptr0, len0);
+    if (ret[1]) {
+        throw takeFromExternrefTable0(ret[0]);
+    }
+}
+
+/**
+ * @returns {boolean}
+ */
+export function is_finished() {
+    const ret = wasm.is_finished();
+    return ret !== 0;
+}
+
+/**
+ * @returns {boolean}
+ */
+export function is_graded() {
+    const ret = wasm.is_graded();
+    return ret !== 0;
+}
+
+export function next_question() {
+    wasm.next_question();
+}
+
+export function previous_question() {
+    wasm.previous_question();
+}
+
+/**
+ * @param {number} current_index
+ * @param {string} selections_json
+ * @param {boolean} is_graded
+ */
+export function restore_quiz_state(current_index, selections_json, is_graded) {
+    const ptr0 = passStringToWasm0(selections_json, wasm.__wbindgen_malloc, wasm.__wbindgen_realloc);
+    const len0 = WASM_VECTOR_LEN;
+    const ret = wasm.restore_quiz_state(current_index, ptr0, len0, is_graded);
+    if (ret[1]) {
+        throw takeFromExternrefTable0(ret[0]);
+    }
+}
+
+/**
+ * @param {number} index
+ */
+export function select_answer(index) {
+    wasm.select_answer(index);
+}
+
+/**
+ * @param {number} index
+ */
+export function set_question_index(index) {
+    wasm.set_question_index(index);
+}
+function __wbg_get_imports() {
+    const import0 = {
+        __proto__: null,
+        __wbindgen_cast_0000000000000001: function(arg0, arg1) {
+            // Cast intrinsic for `Ref(String) -> Externref`.
+            const ret = getStringFromWasm0(arg0, arg1);
+            return ret;
+        },
+        __wbindgen_init_externref_table: function() {
+            const table = wasm.__wbindgen_externrefs;
+            const offset = table.grow(4);
+            table.set(0, undefined);
+            table.set(offset + 0, undefined);
+            table.set(offset + 1, null);
+            table.set(offset + 2, true);
+            table.set(offset + 3, false);
+        },
+    };
+    return {
+        __proto__: null,
+        "./lern_bg.js": import0,
+    };
+}
+
+function getArrayU32FromWasm0(ptr, len) {
+    ptr = ptr >>> 0;
+    return getUint32ArrayMemory0().subarray(ptr / 4, ptr / 4 + len);
+}
+
+function getStringFromWasm0(ptr, len) {
+    return decodeText(ptr >>> 0, len);
+}
+
+let cachedUint32ArrayMemory0 = null;
+function getUint32ArrayMemory0() {
+    if (cachedUint32ArrayMemory0 === null || cachedUint32ArrayMemory0.byteLength === 0) {
+        cachedUint32ArrayMemory0 = new Uint32Array(wasm.memory.buffer);
+    }
+    return cachedUint32ArrayMemory0;
+}
+
+let cachedUint8ArrayMemory0 = null;
+function getUint8ArrayMemory0() {
+    if (cachedUint8ArrayMemory0 === null || cachedUint8ArrayMemory0.byteLength === 0) {
+        cachedUint8ArrayMemory0 = new Uint8Array(wasm.memory.buffer);
+    }
+    return cachedUint8ArrayMemory0;
+}
+
+function passStringToWasm0(arg, malloc, realloc) {
+    if (realloc === undefined) {
+        const buf = cachedTextEncoder.encode(arg);
+        const ptr = malloc(buf.length, 1) >>> 0;
+        getUint8ArrayMemory0().subarray(ptr, ptr + buf.length).set(buf);
+        WASM_VECTOR_LEN = buf.length;
+        return ptr;
+    }
+
+    let len = arg.length;
+    let ptr = malloc(len, 1) >>> 0;
+
+    const mem = getUint8ArrayMemory0();
+
+    let offset = 0;
+
+    for (; offset < len; offset++) {
+        const code = arg.charCodeAt(offset);
+        if (code > 0x7F) break;
+        mem[ptr + offset] = code;
+    }
+    if (offset !== len) {
+        if (offset !== 0) {
+            arg = arg.slice(offset);
+        }
+        ptr = realloc(ptr, len, len = offset + arg.length * 3, 1) >>> 0;
+        const view = getUint8ArrayMemory0().subarray(ptr + offset, ptr + len);
+        const ret = cachedTextEncoder.encodeInto(arg, view);
+
+        offset += ret.written;
+        ptr = realloc(ptr, len, offset, 1) >>> 0;
+    }
+
+    WASM_VECTOR_LEN = offset;
+    return ptr;
+}
+
+function takeFromExternrefTable0(idx) {
+    const value = wasm.__wbindgen_externrefs.get(idx);
+    wasm.__externref_table_dealloc(idx);
+    return value;
+}
+
+let cachedTextDecoder = new TextDecoder('utf-8', { ignoreBOM: true, fatal: true });
+cachedTextDecoder.decode();
+const MAX_SAFARI_DECODE_BYTES = 2146435072;
+let numBytesDecoded = 0;
+function decodeText(ptr, len) {
+    numBytesDecoded += len;
+    if (numBytesDecoded >= MAX_SAFARI_DECODE_BYTES) {
+        cachedTextDecoder = new TextDecoder('utf-8', { ignoreBOM: true, fatal: true });
+        cachedTextDecoder.decode();
+        numBytesDecoded = len;
+    }
+    return cachedTextDecoder.decode(getUint8ArrayMemory0().subarray(ptr, ptr + len));
+}
+
+const cachedTextEncoder = new TextEncoder();
+
+if (!('encodeInto' in cachedTextEncoder)) {
+    cachedTextEncoder.encodeInto = function (arg, view) {
+        const buf = cachedTextEncoder.encode(arg);
+        view.set(buf);
+        return {
+            read: arg.length,
+            written: buf.length
+        };
+    };
+}
+
+let WASM_VECTOR_LEN = 0;
+
+let wasmModule, wasmInstance, wasm;
+function __wbg_finalize_init(instance, module) {
+    wasmInstance = instance;
+    wasm = instance.exports;
+    wasmModule = module;
+    cachedUint32ArrayMemory0 = null;
+    cachedUint8ArrayMemory0 = null;
+    wasm.__wbindgen_start();
+    return wasm;
+}
+
+async function __wbg_load(module, imports) {
+    if (typeof Response === 'function' && module instanceof Response) {
+        if (typeof WebAssembly.instantiateStreaming === 'function') {
+            try {
+                return await WebAssembly.instantiateStreaming(module, imports);
+            } catch (e) {
+                const validResponse = module.ok && expectedResponseType(module.type);
+
+                if (validResponse && module.headers.get('Content-Type') !== 'application/wasm') {
+                    console.warn("`WebAssembly.instantiateStreaming` failed because your server does not serve Wasm with `application/wasm` MIME type. Falling back to `WebAssembly.instantiate` which is slower. Original error:\n", e);
+
+                } else { throw e; }
+            }
+        }
+
+        const bytes = await module.arrayBuffer();
+        return await WebAssembly.instantiate(bytes, imports);
+    } else {
+        const instance = await WebAssembly.instantiate(module, imports);
+
+        if (instance instanceof WebAssembly.Instance) {
+            return { instance, module };
+        } else {
+            return instance;
+        }
+    }
+
+    function expectedResponseType(type) {
+        switch (type) {
+            case 'basic': case 'cors': case 'default': return true;
+        }
+        return false;
+    }
+}
+
+function initSync(module) {
+    if (wasm !== undefined) return wasm;
+
+
+    if (module !== undefined) {
+        if (Object.getPrototypeOf(module) === Object.prototype) {
+            ({module} = module)
+        } else {
+            console.warn('using deprecated parameters for `initSync()`; pass a single object instead')
+        }
+    }
+
+    const imports = __wbg_get_imports();
+    if (!(module instanceof WebAssembly.Module)) {
+        module = new WebAssembly.Module(module);
+    }
+    const instance = new WebAssembly.Instance(module, imports);
+    return __wbg_finalize_init(instance, module);
+}
+
+async function __wbg_init(module_or_path) {
+    if (wasm !== undefined) return wasm;
+
+
+    if (module_or_path !== undefined) {
+        if (Object.getPrototypeOf(module_or_path) === Object.prototype) {
+            ({module_or_path} = module_or_path)
+        } else {
+            console.warn('using deprecated parameters for the initialization function; pass a single object instead')
+        }
+    }
+
+    if (module_or_path === undefined) {
+        module_or_path = new URL('lern_bg.wasm', import.meta.url);
+    }
+    const imports = __wbg_get_imports();
+
+    if (typeof module_or_path === 'string' || (typeof Request === 'function' && module_or_path instanceof Request) || (typeof URL === 'function' && module_or_path instanceof URL)) {
+        module_or_path = fetch(module_or_path);
+    }
+
+    const { instance, module } = await __wbg_load(await module_or_path, imports);
+
+    return __wbg_finalize_init(instance, module);
+}
+
+export { initSync, __wbg_init as default };
